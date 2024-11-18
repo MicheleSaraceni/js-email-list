f652bda04e7c7a9c3920fb268f70d129f84bc53e
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-18 16:34:45 +0100

    cerco di pushare le mail otteniute in un array

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 - Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 */
 
+/***********************************************************/
+
+const list = document.getElementById("list");
+console.log(list);
+let arrayMail = [];
+
+for (let i = 0; i <= 10; i++){
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((mail) => {
+        console.log(mail.data.response);
+        arrayMail.push = mail.data.response;
+      });
+}
+console.log(arrayMail);
+
+//`<li>${mail}</li>`;
